c38bc9a18b8123c8f667ec1b65ab15af28af40a6
anandu <anandu@gamezop.co>
2018-09-10 18:05:07 +0530

    minor

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function handleGameplay(sender_psid,receivedMessage){
 
 		shell.exec(fbpost); */
 		
-	let response;
+/* 	let response;
 	response={
 		"attachement":{
 			"type":"template",
@@ -114,7 +114,54 @@ function handleGameplay(sender_psid,receivedMessage){
 			}
 		}
 	}
-	callSendAPI(sender_psid,response);
+	callSendAPI(sender_psid,response); */
+var headers = {
+    'Content-Type': 'application/json'
+};
+
+var dataString = `{
+  "messaging_type": "UPDATE",
+  "recipient": {
+    "id": ${sender_psid}
+  },
+  "message": {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [
+          {
+            "title": "It has been a while since your last game. Time to get back",
+            "buttons": [
+              {
+                "type": "game_play",
+                "title": "Play Tic-Tac-Toe.",
+                "payload": "{}",
+                "game_metadata": {
+                }
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
+}`;
+
+var options = {
+    url: 'https://graph.facebook.com/v2.6/me/messages?access_token='+process.env.PAGE_ACCESS_TOKEN,
+    method: 'POST',
+    headers: headers,
+    body: dataString
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
+}
+
+request(options, callback);
 };
 function handlePostback(sender_psid,received_postback){
 
